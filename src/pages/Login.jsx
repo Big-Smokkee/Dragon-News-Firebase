6a3ab89424c8cec0f6722839a04f9e1c6feacdc5
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-29 19:07:19 +0600

    51-4 Login Functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../provider/AuthProvider';
 
 const Login = () => {
+    const { setUser, signInUser } = use(AuthContext);
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log({ email, password });
+        signInUser(email, password)
+            .then(res => {
+                alert("Login Done");
+                setUser(res.user);
+                console.log(res.user);
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
+    }
     return (
         <div className='flex justify-center min-h-screen items-center'>
-
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto py-5">
                 <h2 className='font-semibold text-2xl text-center'>Login your account</h2>
-                <div className="card-body">
+                <form onSubmit={handleLogin} className="card-body">
                     <fieldset className="fieldset">
                         <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
+                        <input name='email' type="email" className="input" placeholder="Email" />
                         <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
+                        <input name='password' type="password" className="input" placeholder="Password" />
                         <div><a className="link link-hover">Forgot password?</a></div>
-                        <button className="btn btn-neutral mt-4">Login</button>
+                        <button type='submit' className="btn btn-neutral mt-4">Login</button>
                     </fieldset>
                     <p className='font-semibold text-center mt-7'>Don't Have An Account? <Link to="/auth/register" className='text-secondary'>Register</Link></p>
-                </div>
+                </form>
             </div>
         </div>
     );
