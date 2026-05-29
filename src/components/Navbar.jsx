294324efe403cb3a3963d4e92e24e925757934b3
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-29 18:16:09 +0600

    had to change the user.pass to user.email. cz there is no propoerty in the user as pass

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const { user } = use(AuthContext);
   return (
     <div className="flex justify-between items-center">
-      <div className="">{user && user.pass}</div>
+      <div className="">{user && user.email}</div>
       <div className="nav flex gap-5 text-accent">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
