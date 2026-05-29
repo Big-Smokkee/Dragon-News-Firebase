fe56cb8ce6bd665d08ffc4e337a2f5c2a3b65101
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-29 19:08:22 +0600

    51 - 4 LogOut as signOutUser & Login as SignInUser Functionality

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from 'react';
 import { app } from '../firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
@@ -8,8 +9,17 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     console.log(user);
+
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signOutUser = () => {
+        return signOut(auth);
+    }
+
+    const signInUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
     }
 
     useEffect(() => {
@@ -24,7 +34,9 @@ const AuthProvider = ({ children }) => {
     const authData = {
         user,
         setUser,
-        createUser
+        createUser,
+        signOutUser,
+        signInUser
     }
     return <AuthContext value={authData}>
         {children}
