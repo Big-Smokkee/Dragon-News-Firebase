09e5297f38c2337d7e5cecfc5b4a6eaddf0c8b30
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-30 11:24:05 +0600

    51-7 Handle validation Errors

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,34 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import { Link } from 'react-router';
 import { AuthContext } from '../provider/AuthProvider';
 
 const Register = () => {
     const { setUser, createUser } = use(AuthContext);
+    const [nameError, setNameError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+
     const handleRegister = (e) => {
         e.preventDefault();
         const form = e.target;
         const name = form.name.value;
+
+        if (name.length < 3) {
+            setNameError("Name should be more than 5 characters");
+            return;
+        }
+        else {
+            setNameError("");
+        }
         const photoURL = form.photoURL.value;
         const email = form.email.value;
         const password = form.password.value;
+        if (password.length < 6) {
+            setPasswordError("Password length must be more than 5");
+            return;
+        }
+        else {
+            setPasswordError("");
+        }
         console.log({ name, photoURL, email, password });
         createUser(email, password)
             .then((res) => {
@@ -32,15 +50,19 @@ const Register = () => {
                         {/* name */}
                         <label className="label">Your Name</label>
                         <input name='name' type="text" className="input" placeholder="Enter your name" required />
+                        {
+                            nameError && <p className='text-xs text-red-600'>{nameError}</p>
+                        }
                         {/* Photo */}
                         <label className="label">Photo URL</label>
                         <input name='photoURL' type="text" className="input" placeholder="Enter your photo URL" required />
                         {/* email */}
                         <label className="label">Email</label>
                         <input name='email' type="email" className="input" placeholder="Enter your email address" required />
+                        {/* password */}
                         <label className="label">Password</label>
                         <input name='password' type="password" className="input" placeholder="Enter your pasword" required />
-                        <p></p>
+                        {passwordError && <p className='text-xs text-red-600 font-semibold'>{passwordError}</p>}
                         <button type='submit' className="btn btn-neutral mt-4">Register</button>
                     </fieldset>
                     <p className='font-semibold text-center mt-7'>Already Have An Account? <Link to="/auth/login" className='text-secondary'>Login</Link></p>
