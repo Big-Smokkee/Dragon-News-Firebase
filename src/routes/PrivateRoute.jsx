b1054379dcd3129e6b1fe892b92c1fbb1dc07b77
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-30 11:24:32 +0600

    51-7 Implement Redirects for Login & Handle validation Errors

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import React, { use } from 'react';
 import { AuthContext } from '../provider/AuthProvider';
-import { Navigate } from 'react-router';
+import { Navigate, useLocation } from 'react-router';
 import Loading from '../pages/Loading';
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = use(AuthContext);
-
+    const location = useLocation();
+    console.log(location);
     if (loading) {
         return <Loading></Loading>
     }
@@ -16,7 +17,7 @@ const PrivateRoute = ({ children }) => {
 
     //else go to login
     else
-        return <Navigate to='/auth/login'></Navigate>
+        return <Navigate state={location.pathname} to='/auth/login'></Navigate>
 };
 
 export default PrivateRoute;
